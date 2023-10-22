9373587f9d75dd4888b6a1acd2bc356ddd1c9d88
Eddie <eddie@directbox.de>
2023-10-22 21:35:46 +0200

    autokey cleanup

--- autokey/data/BRAVE/Alt C.py
+++
@@ -1,4 +0,0 @@
-if "Alacritty.Alacritty" in window.get_active_class():
-    keyboard.send_keys("<ctrl>+<shift>+c")
-else:
-    keyboard.send_keys("<ctrl>+c")
--- autokey/data/GENERAL/Alt Q.py
+++ autokey/data/GENERAL/Alt Q.py
@@ -1,4 +1,6 @@
 if "Alacritty.Alacritty" in window.get_active_class():
     keyboard.send_keys("<alt>+q")
+if "xfce4-terminal" in window.get_active_class():
+    keyboard.send_keys("<ctrl>+<shift>+w")
 else:
     keyboard.send_keys("<ctrl>+w")
