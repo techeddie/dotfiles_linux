c8f4de02144d646c134034d92de0d884410d7a65
Eddie <eddie@directbox.de>
2023-10-26 23:57:10 +0200

    gbranch alias gb

--- zsh/fzfgit.zsh
+++ zsh/fzfgit.zsh
@@ -22,6 +22,13 @@ glog() {
 
 # fbr - checkout git branch (including remote branches)
 #fuzzy show branches
+gb() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
 gbranch() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
