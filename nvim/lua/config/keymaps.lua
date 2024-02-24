9ff10f594a377e99bd0334f2f929bb1f0cc96089
Eddie <eddie@directbox.de>
2024-02-24 20:43:06 +0100

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
-local function keymap(mode, lhs, rhs, opts)
+  local function keymap(mode, lhs, rhs, opts)
   local options = { noremap = true }
   if opts then options = vim.tbl_extend('force', options, opts) end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
@@ -147,7 +147,6 @@ local opts = { noremap = true, silent = true }
 
 --LAZYGIT
   keymap("n", "<M-g>", ":LazyGitCurrentFile<CR>") --lazygit
-  keymap("n", "<M-p>", ":LazyGitCurrentFile<CR>") --lazygit
 
 --SEARCH AND REPLACE
   -- keymap("n", "<C-f>", ":SearchBoxSimple<CR>")
