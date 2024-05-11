91f923512c2a1b2127e33afe198763ccf25b1bac
Eddie <eddie@directbox.de>
2024-05-11 15:18:15 +0200

    find files root = config files

--- nvim/lua/plugconfig/alpha.lua
+++ nvim/lua/plugconfig/alpha.lua
@@ -6,7 +6,7 @@ end
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {"neovim alpha"}
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files cwd=~/.config/<CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("g", "  Find text", ":Telescope live_grep <CR>"),
