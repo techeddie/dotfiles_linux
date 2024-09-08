5cfe966b1db4f4596361d6df81e28cf72c9abb66
Eddie <eddie@directbox.de>
2024-09-08 21:53:35 +0200

    keymap mods

--- autokey/data/GENERAL/Alt J.py
+++ autokey/data/GENERAL/Alt J.py
@@ -1,7 +1,5 @@
 winClass = window.get_active_class()
 if "Alacritty.Alacritty" in window.get_active_class():
     keyboard.send_keys("<alt>+j")
-if "Brave-browser" in winClass:    
-    keyboard.send_keys("<ctrl>+<page_down>")
 else:
     keyboard.send_keys("<ctrl>+<page_up>")
