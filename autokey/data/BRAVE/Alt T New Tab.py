380ce76af6b3b27d3344830d26c299005c3f77f0
Eddie <eddie@directbox.de>
2024-09-11 22:50:48 +0200

    update

--- autokey/data/BRAVE/Alt T New Tab.py
+++ autokey/data/BRAVE/Alt T New Tab.py
@@ -3,6 +3,8 @@ winClass = window.get_active_class()
 if "Brave-browser" in winClass:    
     keyboard.send_keys("<ctrl>+t")
 if "Doublecmd" in winClass:    
-    keyboard.send_keys("<alt>+t")    
+    keyboard.send_keys("<alt>+t")
+if "Alacritty.Alacritty" in winClass:    
+    keyboard.send_keys("<alt>+t")   
 else:
     keyboard.send_keys("<alt>+n")
