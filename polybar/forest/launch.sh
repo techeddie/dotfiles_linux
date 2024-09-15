505b1cf41598654db4ba4f5be5c682ce2e2b7215
Eddie <eddie@directbox.de>
2024-09-15 14:21:13 +0200

    testing

--- polybar/forest/launch.sh
+++ polybar/forest/launch.sh
@@ -12,3 +12,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 polybar -q main -c "$DIR"/config.ini &
+
+#launch my apps
+nitrogen --restore
+sh $HOME/scripts/runMouseGXTAccel.sh
+sh $HOME/scripts/runHDMI_AUTODETECT.sh
