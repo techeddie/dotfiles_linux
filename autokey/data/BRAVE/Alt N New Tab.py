c74bd912838fc98ea6e37d1d41f3e8f9ef759c5d
Eddie <eddie@directbox.de>
2024-09-15 14:21:07 +0200

    autokey maps

---
+++ autokey/data/BRAVE/Alt N New Tab.py
@@ -0,0 +1,10 @@
+winClass = window.get_active_class()
+
+if "Brave-browser" in winClass:    
+    keyboard.send_keys("<ctrl>+t")
+if "Doublecmd" in winClass:    
+    keyboard.send_keys("<alt>+t")
+if "Alacritty.Alacritty" in winClass:    
+    keyboard.send_keys("<alt>+n")   
+else:
+    keyboard.send_keys("<alt>+n")
