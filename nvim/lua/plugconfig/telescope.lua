7cbffa794d23c8cf1d2b5f2e8d2d5113f3b3d678
Eddie <eddie@directbox.de>
2024-10-25 19:20:02 +0200

    c-f and m-f switched

--- nvim/lua/plugconfig/telescope.lua
+++ nvim/lua/plugconfig/telescope.lua
@@ -21,7 +21,7 @@ local actions = require "telescope.actions"
     })
   end, { desc = '[/] Fuzzily search in current buffer]' })
 
-  vim.keymap.set('n', '<C-f>', function()
+  vim.keymap.set('n', '<M-f>', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
     require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
       winblend = 10,
