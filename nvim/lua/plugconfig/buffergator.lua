2f365a6c52750e856bf54ad916371fd558d7bd76
Eddie <eddie@directbox.de>
2025-01-12 12:57:57 +0100

    bufname off

--- nvim/lua/plugconfig/buffergator.lua
+++ nvim/lua/plugconfig/buffergator.lua
@@ -8,9 +8,8 @@ g.buffergator_split_size=10
 g.buffergator_vsplit_size=30
 g.buffergator_hsplit_size=30
 
-
 g.buffergator_autodismiss_on_select = 0
-g.buffergator_display_regime = "bufname"
+-- g.buffergator_display_regime = "bufname"
 -- g.buffergator_sort_regime    = "filepath"
 g.buffergator_autoupdate = 1
 
