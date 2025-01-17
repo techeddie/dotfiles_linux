35c6be413e5701d90d87fb8bb748eca43c7f2773
Eddie <eddie@directbox.de>
2025-01-17 22:34:38 +0100

    preview disabled

--- nvim/lua/plugconfig/fzf.lua
+++ nvim/lua/plugconfig/fzf.lua
@@ -8,10 +8,12 @@ let g:fzf_action = {
 			\ 'ctrl-v': 'vsplit' }
 
 let g:fzf_layout = { 'down':  '40%'}
+let g:fzf_preview_window = ''
 
 let g:fzf_tags_command = 'ctags -R'
 	let $FZF_DEFAULT_OPTS = '--layout=reverse --info=inline'
 	let $FZF_DEFAULT_COMMAND="rg --files --hidden"
+
 	" Get Files
 	command! -bang -nargs=? -complete=dir Files
 			\ call fzf#vim#files(<q-args>, fzf#vim#with_preview({'options': ['--layout=reverse', '--info=inline']}), <bang>0)
