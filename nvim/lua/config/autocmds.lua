d8275557a4fce454b336f0d49f9f3694370a258e
Eddie <eddie@directbox.de>
2025-01-19 22:24:49 +0100

    init lazyvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,44 +1,8 @@
--- AUTOCOMMANDS
-local M = {}
-function M.autocmd(event, triggers, operations)
-  local cmd = string.format("autocmd %s %s %s", event, triggers, operations)
- vim.cmd(cmd)
-end
-
-M.autocmd("TermOpen",     "*",   "setl bufhidden=hide")
-M.autocmd("TermOpen",     "*",   "startinsert")
-M.autocmd("TermOpen",     "*",   "setl nonumber")
-M.autocmd("BufEnter",     "*",   "silent! lcd %:p:h")
-
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 --
-local cmd = vim.cmd
-
-cmd [[
-  "AUTOCOMMANDS"
-    set statusline+=%{get(b:,'gitsigns_status','')}
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd BufWritePost keymaps.lua source ~/.config/nvim/lua/config/keymaps.lua"
-    autocmd BufWritePost autocmds.lua source ~/.config/nvim/lua/config/autocmds.lua"
-    autocmd BufWritePost colorscheme.lua source ~/.config/nvim/lua/config/colorscheme.lua"
-    autocmd BufWritePost telescope.lua source ~/.config/nvim/lua/plugconfig/telescope.lua"
-    autocmd BufWritePost settings.lua source ~/.config/nvim/lua/config/settings.lua"
-
-    autocmd FileType sh nmap <F5> <ESC>:w<CR>:ter zsh "%"<CR>
-    "autocmd FileType sh nmap <F8> <ESC>:.w !bash<cr>
-    "autocmd FileType sh vmap <F8> <ESC>:.w !bash<cr>
-    "autocmd FileType lua nmap <F5> <ESC>:w<CR>:ter lua "%"<CR>
-
-    "autocmd FileType lua nmap <F5> :TermExec cmd="lua %:p" direction=horizontal<cr>
-    "autocmd FileType lua nmap <F5> <ESC>:w<cr> :!lua5.4 %<cr>
-    "autocmd BufReadPost,FileReadPost * normal zR
-
-    "FORMAT ON SAVE
-    " autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
-    ""-- autocmd BufWritePre * sh vim.lsp.buf.formatting_sync()
-
-    "CHANGE PWD WHEN OPENING A FILE - causing issue with toggleterm and autopairs
-     "autocmd BufEnter * if &buftype =="toggleterm" | lcd %:p:h 
-
-    "CHANGE PWD WHEN OPENING A FILE - TESTING
-       autocmd BufEnter * silent! lcd %:p:h
-]]
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
