595528f141ef589818f6f2a4fdf52ea2c7f35d77
Eddie <eddie@directbox.de>
2025-03-07 19:48:18 +0100

    bookmark toggle

--- autokey/data/BRAVE/Alt 1 bookmars toggle1.py
+++ autokey/data/BRAVE/Alt 1 bookmars toggle1.py
@@ -1,6 +1,6 @@
 winClass = window.get_active_class()
 
 if "Brave-browser" in winClass:    
-    keyboard.send_keys("<alt>+b")    
+    keyboard.send_keys("<ctrl>+<shift>+b")    
 else:
     keyboard.send_keys("<alt>+1")
