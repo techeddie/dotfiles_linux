7f743af9db07a040a441c3d9af7c13acf9ff5c88
Eddie <eddie@directbox.de>
2025-03-09 23:18:15 +0100

    lf added

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,3 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-require("config.startify")
--- nvim/lua/plugins/lf.lua
+++ nvim/lua/plugins/lf.lua
@@ -1,6 +1,24 @@
 return {
-  "ptzz/lf.vim",
+  "lmburns/lf.nvim",
   config = function()
-    require("lf").setup({ "*" })
+    -- This feature will not work if the plugin is lazy-loaded
+    vim.g.lf_netrw = 1
+
+    require("lf").setup({
+      escape_quit = false,
+      border = "rounded",
+    })
+
+    vim.keymap.set("n", "<M-l>", "<Cmd>Lf<CR>")
+    vim.keymap.set("n", "<leader>lf", "<Cmd>Lf<CR>")
+
+    vim.api.nvim_create_autocmd({
+      event = "User",
+      pattern = "LfTermEnter",
+      callback = function(a)
+        vim.api.nvim_buf_set_keymap(a.buf, "t", "q", "q", { nowait = true })
+      end,
+    })
   end,
+  requires = { "toggleterm.nvim" },
 }
