c91b6ae5d56d6ff3a1f63c6a8aed68ef12c575bd
Eddie <eddie@directbox.de>
2025-03-15 00:23:04 +0100

    lf customizing

--- nvim/lua/plugins/lf.lua
+++ nvim/lua/plugins/lf.lua
@@ -1,26 +1,24 @@
 return {
-  -- https://github.com/lmburns/lf.nvim
   "lmburns/lf.nvim",
-  config = function()
-    -- This feature will not work if the plugin is lazy-loaded
-    vim.g.lf_netrw = 1
-
-    require("lf").setup({
-      escape_quit = false,
-      default_action = "drop", -- default action when `Lf` opens a file
-      dir = "", -- directory where `lf` starts ('gwd' is git-working-directory, ""/nil is CWD)
-      direction = "float", -- window type: float horizontal vertical
-      border = "rounded", -- border kind: single double shadow curved
-      focus_on_open = true, -- focus the current file when opening Lf (experimental)
-      mappings = true, -- whether terminal buffer mapping is enabled
-      tmux = false, -- tmux statusline can be disabled on opening of Lf
-      default_file_manager = false, -- make lf default file manager
-      disable_netrw_warning = true, -- don't display a message when opening a directory with `default_file_manager` as true
-      -- winblend = 10, -- psuedotransparency level
-    })
-
-    vim.keymap.set("n", "<M-l>", "<Cmd>Lf<CR>")
-    vim.keymap.set("n", "<leader>lf", "<Cmd>Lf<CR>")
-  end,
-  requires = { "toggleterm.nvim" },
+  cmd = "Lf",
+  dependencies = { "nvim-lua/plenary.nvim", "akinsho/toggleterm.nvim" },
+  opts = {
+    highlights = { NormalFloat = { guibg = "NONE" } },
+    border = "single", --rounded
+    escape_quit = true,
+    lf_netrw = 1,
+    default_action = "drop",
+    dir = "",
+    direction = "float",
+    focus_on_open = true,
+    mappings = true,
+    tmux = false,
+    default_file_manager = false,
+    disable_netrw_warning = true,
+    winblend = 5,
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Lf<cr>", desc = "NeoTree" },
+    { "<M-l>", "<cmd>Lf<cr>", desc = "NeoTree" },
+  },
 }
