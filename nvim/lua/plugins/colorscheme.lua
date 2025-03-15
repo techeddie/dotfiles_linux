c47d9bf65388a92be55c7451106308a44af73430
Eddie <eddie@directbox.de>
2025-03-15 01:51:38 +0100

    kill

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,4 +5,10 @@ return {
   config = function()
     vim.cmd("colorscheme tokyodark")
   end,
+  keys = function()
+    return {
+      { "<C-S-k>", ":qall! <CR>" },
+      { "<C-S-q>", ":<C-w>q<CR>" },
+    }
+  end,
 }
