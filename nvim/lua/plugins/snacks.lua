d58ddd22053084d9d693d4d800e22128b16a8e9f
Eddie <eddie@mbp.local>
2025-04-03 03:22:21 +0200

    customer keymaps

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/snacks.nvim",
+    keys = {
+			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent" },
+      { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>ll", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+   }
+}
