2453ec55cb375d5595f5f2e3b9a1fa70118d0170
Eddie <eddie@mbp.local>
2025-04-03 03:32:23 +0200

    moved to snacks

--- nvim/lua/plugins/customs.lua
+++ nvim/lua/plugins/customs.lua
@@ -8,9 +8,6 @@ return {
 	keys = function()
 		return {
 			{ "<C-k>", ":qall! <CR>" },
-			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent", },
-      { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-      { "<leader>ll", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
 		}
 	end,
 }
