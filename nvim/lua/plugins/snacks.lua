eaa80f18e115c4b4c87d6a08eb0d5d51aa8ed1e7
Eddie <eddie@mbp.local>
2025-04-05 23:57:06 +0200

    bufferlines

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   "folke/snacks.nvim",
     keys = {
 			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent" },
-      { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      -- { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
       { "<leader>ll", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
    }
 }
