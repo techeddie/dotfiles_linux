e1173b10ee3dfaacc3a15e794bef03d4ec88853f
Eddie <eddie@directbox.de>
2025-04-14 22:59:40 +0200

    leader space fold under cursor

--- nvim/lua/plugins/customs.lua
+++ nvim/lua/plugins/customs.lua
@@ -7,9 +7,10 @@ return {
 	end,
 	keys = function()
 		return {
-			{ "<C-k>", ":qall! <CR>" },
-      { "<C-g>", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" }},
-      { "<C-f>", function() Snacks.picker.lines() end, { desc = "Buffer lines" }},
+			{ "<C-k>", ":qall! <CR>" }, --close nvim
+			{ "<leader><space>", "za" }, --fold under cursor
+      { "<C-g>", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" }}, --lazygit
+      { "<C-f>", function() Snacks.picker.lines() end, { desc = "Buffer lines" }}, --find text in current file via bufferline
 		}
 	end,
 }
