8c7798cdbef6923d480c12f809eb8d715ca49b11
Eddie <eddie@directbox.de>
2025-04-17 21:47:14 +0200

    ghostty alt q(uit)

--- autokey/data/GENERAL/Alt Q.py
+++ autokey/data/GENERAL/Alt Q.py
@@ -1,5 +1,7 @@
 if "Alacritty" in window.get_active_class():
     keyboard.send_keys("<alt>+q")
+if "ghostty" in window.get_active_class():
+    keyboard.send_keys("<alt>+q")
 if "xfce4-terminal" in window.get_active_class():
     keyboard.send_keys("<ctrl>+d")
 else:
