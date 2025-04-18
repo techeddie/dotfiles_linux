17a3a4fceb298ee8e03e1fe9d210a7079337b801
Eddie <eddie@directbox.de>
2025-04-18 23:50:36 +0200

    testing ghostty terminal
    issues closing pane

--- autokey/data/GENERAL/Alt Q.py
+++ autokey/data/GENERAL/Alt Q.py
@@ -1,7 +1,7 @@
 if "Alacritty" in window.get_active_class():
     keyboard.send_keys("<alt>+q")
-if "ghostty" in window.get_active_class():
-    keyboard.send_keys("<alt>+q")
+if "com.mitchellh.ghostty" in window.get_active_class():
+    keyboard.send_keys("<ctrl>+<shift>+q")    
 if "xfce4-terminal" in window.get_active_class():
     keyboard.send_keys("<ctrl>+d")
 else:
