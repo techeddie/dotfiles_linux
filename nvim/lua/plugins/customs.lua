a0d8cd95b2b1cae777bf0c5e6dd31dfbff91920c
Eddie <eddie@directbox.de>
2025-04-20 21:50:03 +0200

    leader nn = new line

--- nvim/lua/plugins/customs.lua
+++ nvim/lua/plugins/customs.lua
@@ -9,6 +9,7 @@ return {
 		return {
 			{ "<C-k>", ":qall! <CR>" }, --close nvim
 			{ "<leader><space>", "za" }, --fold under cursor
+			{ "<leader>nn", ":a<CR><CR>.<CR>" }, --insert a newline without entering in insert mode
       { "<C-g>", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" }}, --lazygit
       { "<C-f>", function() Snacks.picker.lines() end, { desc = "Buffer lines" }}, --find text in current file via bufferline
 		}
