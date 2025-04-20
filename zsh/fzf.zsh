1bb87240ab39f545034e2eac09bb752b16b5de13
Eddie <eddie@directbox.de>
2025-04-20 23:11:15 +0200

    theming

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,14 @@
-# -- Use fd instead of fzf --
+# THEMING
+  # mocha
+    # https://github.com/catppuccin/fzf
+    export FZF_DEFAULT_OPTS=" \
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+    --color=selected-bg:#45475a \
+    --color=border:#313244,label:#cdd6f4"
+
+# USE FD INSTEAD OF FZF
   export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
@@ -14,3 +24,21 @@
   _fzf_compgen_dir() {
     fd --type=d --hidden --exclude .git . "$1"
   }
+
+
+# THEME ALTERNATIVES
+  # fzf web color picker
+    # https://minsw.github.io/fzf-color-picker/
+      # export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#d0d0d0,bg:#121212,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf'
+
+  # josean
+    # https://www.josean.com/posts/7-amazing-cli-tools
+    # fg="#CBE0F0"
+    # bg="#011628"
+    # bg_highlight="#143652"
+    # purple="#B388FF"
+    # blue="#06BCE4"
+    # cyan="#2CF9ED"
+    #
+    # export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
+
