355d952d4dd2d88b7c01f26013bc1b0e9d7d7563
Eddie <eddie@directbox.de>
2025-04-25 00:22:58 +0200

    playing around

--- yazi/init.lua
+++ yazi/init.lua
@@ -11,6 +11,8 @@ require("session"):setup {
 	sync_yanked = true,
 }
 
+
+
 Status:children_add(function(self)
 	local h = self._current.hovered
 	if h and h.link_to then
@@ -19,3 +21,47 @@ Status:children_add(function(self)
 		return ""
 	end
 end, 3300, Status.LEFT)
+
+
+
+-- -- Show the path of the currently hovered file in the header
+-- function Header:cwd()
+-- 	local max = self._area.w - self._right_width
+-- 	if max <= 0 then
+-- 		return ui.Span("")
+-- 	end
+--
+-- 	local cwd = ya.readable_path(tostring(self._tab.current.cwd)) .. self:flags()
+-- 	local left = ui.Line {
+--     ui.Span(cwd):fg("blue"):bold(true),
+--     ui.Span("/"):fg("blue"):bold(true),
+--     ui.Span(tostring(cx.active.current.hovered.name)):fg("blue"):   bold(true),
+--   }
+--
+--   return left
+--
+-- end
+
+
+-- Show username and hostname in header
+-- Header:children_add(function()
+-- 	if ya.target_family() ~= "unix" then
+-- 		return ui.Line {}
+-- 	end
+-- 	return ui.Line { ui.Span(ya.user_name() .. "@" .. ya.host_name()):fg("lightgreen"):bold(true), ui.Span(":") }
+-- end, 500, Header.LEFT)
+
+
+-- Show symlink path in status bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line {}
+	end
+
+  local linked = ""
+ 	if h.link_to ~= nil then
+ 		linked = " -> " .. tostring(h.link_to)
+ 	end
+ 	return ui.Line(" " .. h.name .. linked)
+end
