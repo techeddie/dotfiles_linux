3cdc89ccb14f4899ab2849c4112af519c2ddd1cf
Eddie <eddie@directbox.de>
2025-05-10 00:31:35 +0200

    ghostty alt n not working

--- autokey/data/BRAVE/Alt N New Tab.py
+++ autokey/data/BRAVE/Alt N New Tab.py
@@ -5,8 +5,14 @@ if "Brave-browser" in winClass:
 if "Doublecmd" in winClass:    
     keyboard.send_keys("<alt>+t")
 if "Alacritty.Alacritty" in winClass:    
-    keyboard.send_keys("<ctrl>+a+c")    
-if "com.mitchellh.ghostty" in winClass:    
-    keyboard.send_keys("<ctrl>+a+c")    
+    # keyboard.send_keys("<alt>+n")
+    keyboard.send_keys("<ctrl>+a")    
+    keyboard.send_keys("c")    
+# if "com.mitchellh.ghostty" in winClass:    
+#     win = window.get_active_class()
+#     print(win)
+    # keyboard.send_keys("<alt>+n")
+    # keyboard.send_keys("<ctrl>+a")    
+    # keyboard.send_keys("c")    
 else:
     keyboard.send_keys("<alt>+n")
