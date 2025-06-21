e8a7823dbb141d5d84c867dba48c8c2952146277
Eddie <eddie@directbox.de>
2025-06-21 21:04:03 +0200

    tmux alt a fix

--- autokey/data/FIREFOX/Alt A Bookmark Sidebar.py
+++ autokey/data/FIREFOX/Alt A Bookmark Sidebar.py
@@ -1,6 +1,9 @@
 winClass = window.get_active_class()
 
 if "firefox" in winClass:    
-    keyboard.send_keys("<ctrl>+b") 
+    keyboard.send_keys("<ctrl>+b")
+if "com.mitchellh.ghostty" in winClass:
+     import subprocess
+     subprocess.run(["tmux", "previous-window"])  
 else:
     keyboard.send_keys("<alt>+a")
