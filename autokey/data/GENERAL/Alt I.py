a85237019e28d624b23cd09ec79a17d51d911349
Eddie <eddie@directbox.de>
2025-06-21 21:03:51 +0200

    alt i u disabled

--- autokey/data/GENERAL/Alt I.py
+++ autokey/data/GENERAL/Alt I.py
@@ -1,4 +1,7 @@
+winClass = window.get_active_class()
 if "Alacritty.Alacritty" in window.get_active_class():
     keyboard.send_keys("<end>")
+if "com.mitchellh.ghostty" in winClass:
+    keyboard.send_keys("<f10>")
 else:
     keyboard.send_keys("<end>")
