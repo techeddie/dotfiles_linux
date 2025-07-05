e4ecb8dfec224b5198636c12e2797db20fa3bbb4
Eddie <eddie@directbox.de>
2025-07-05 19:05:19 +0200

    alt down tab list

--- autokey/data/BRAVE/Alt 2 tab list toggle.py
+++ autokey/data/BRAVE/Alt 2 tab list toggle.py
@@ -3,4 +3,4 @@ winClass = window.get_active_class()
 if "Brave-browser" in winClass:    
     keyboard.send_keys("<ctrl>+<shift>+a")    
 else:
-    keyboard.send_keys("<alt>+2")
+    keyboard.send_keys("<alt>+Down")
