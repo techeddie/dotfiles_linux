afa25b6c3a816d3cbe223fa8a900923e68677b78
Eddie <eddie@directbox.de>
2025-07-06 18:44:50 +0200

    conflict with tmux

--- autokey/data/BRAVE/Alt Down tab list toggle.py
+++ autokey/data/BRAVE/Alt Down tab list toggle.py
@@ -1,6 +1,9 @@
 winClass = window.get_active_class()
 
 if "Brave-browser" in winClass:    
-    keyboard.send_keys("<ctrl>+<shift>+a")    
+    keyboard.send_keys("<ctrl>+<shift>+a")
+if "com.mitchellh.ghostty" in winClass:
+     import subprocess
+     subprocess.run(["tmux", "selectp -D"])
 else:
     keyboard.send_keys("<alt>+Down")
