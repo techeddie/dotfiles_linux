445db2625a89e4093a84576adbef9bb7b66f63d5
Eddie <eddie@directbox.de>
2025-07-12 14:59:12 +0200

    alt dot ghostty testing

--- autokey/data/GENERAL/ALT DOT.py
+++ autokey/data/GENERAL/ALT DOT.py
@@ -1,1 +1,9 @@
-keyboard.send_keys("<escape>")
+winClass = window.get_active_class()
+print("Window class:", winClass)
+
+if "com.mitchellh.ghostty" in winClass:
+    # Terminal expects Alt+. as ESC + .
+    keyboard.send_keys("<escape>")
+#else:
+    # Regular Alt+. might be sent with real modifier elsewhere
+ #   keyboard.send_keys("<alt>.")
