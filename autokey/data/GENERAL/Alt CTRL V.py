cf6705ff6f7b5f0647cc847c8bbe24c31c4e97ce
Eddie <eddie@directbox.de>
2025-08-11 23:54:27 +0200

    autotype clipboard
    proxmox console

---
+++ autokey/data/GENERAL/Alt CTRL V.py
@@ -0,0 +1,14 @@
+import time
+
+# Get clipboard contents
+text = clipboard.get_clipboard()
+
+# Optional: small delay before typing (gives you time to focus window)
+time.sleep(1)
+
+# Type each character with a short delay
+for char in text:
+    keyboard.send_keys(char)
+    time.sleep(0.02)  # 20 ms between keystrokes
+    
+keyboard.send_keys("<enter>")
