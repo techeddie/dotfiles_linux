00967193d478b2b7931bffe2ebc9298f5971952e
Eddie <eddie@directbox.de>
2025-08-20 22:37:21 +0200

    alacritty floating alt q

--- autokey/data/GENERAL/Alt Q.py
+++ autokey/data/GENERAL/Alt Q.py
@@ -1,5 +1,7 @@
 if "Alacritty" in window.get_active_class():
     keyboard.send_keys("<alt>+q")
+if "sp-term2" in window.get_active_class():
+    keyboard.send_keys("<alt>+q")
 if "guake" in window.get_active_class():
     keyboard.send_keys("<alt>+q")    
 if "com.mitchellh.ghostty" in window.get_active_class():
