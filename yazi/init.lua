9990a2ce6a1338182de3f430af44eb61ca59f09c
Eddie <eddie@directbox.de>
2026-01-13 11:10:32 +0300

    projects added

--- yazi/init.lua
+++ yazi/init.lua
@@ -61,3 +61,54 @@ function Linemode:size_and_mtime()
 	local size = self._file:size()
 	return string.format("%s %s", size and ya.readable_size(size) or "", time)
 end
+
+-- projects
+-- https://github.com/MasouShizuka/projects.yazi
+require("projects"):setup({
+    event = {
+        save = {
+            enable = true,
+            name = "project-saved",
+        },
+        load = {
+            enable = true,
+            name = "project-loaded",
+        },
+        delete = {
+            enable = true,
+            name = "project-deleted",
+        },
+        delete_all = {
+            enable = true,
+            name = "project-deleted-all",
+        },
+        merge = {
+            enable = true,
+            name = "project-merged",
+        },
+    },
+    save = {
+        method = "yazi", -- yazi | lua
+        yazi_load_event = "@projects-load", -- event name when loading projects in `yazi` method
+        lua_save_path = "", -- path of saved file in `lua` method, comment out or assign explicitly
+                            -- default value:
+                            -- windows: "%APPDATA%/yazi/state/projects.json"
+                            -- unix: "~/.local/state/yazi/projects.json"
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+        update_before_quit = false,
+        load_after_start = false,
+    },
+    merge = {
+        event = "projects-merge",
+        quit_after_merge = false,
+    },
+    notify = {
+        enable = true,
+        title = "Projects",
+        timeout = 3,
+        level = "info",
+    },
+})
