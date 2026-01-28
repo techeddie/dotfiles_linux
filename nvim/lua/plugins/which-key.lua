1ea558b1cb323b6ef9238782f6947dde7da89c87
Eddie <eddie@directbox.de>
2026-01-28 21:56:39 +0300

    whichkey classic view

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ return
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    --default: preset = "helix",
-    preset = "classic", --show below
+    preset = "helix",
+    -- preset = "classic", --show below
   },
 }
