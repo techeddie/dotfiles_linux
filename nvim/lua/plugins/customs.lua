034dc0cc85d7308ea32cec4bdc81589e7e08db3c
Eddie <eddie@directbox.de>
2026-02-28 21:31:10 +0100

    tokyodark

--- nvim/lua/plugins/customs.lua
+++ nvim/lua/plugins/customs.lua
@@ -3,7 +3,8 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme tokyonight")
+		-- vim.cmd("colorscheme tokyonight")
+		vim.cmd("colorscheme tokyodark")
 	end,
 	keys = function()
 		return {
