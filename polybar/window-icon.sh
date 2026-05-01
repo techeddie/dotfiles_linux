9108872bfc27fc5ba6885ebe1cbbcd6da38a0b56
Eddie <eddie@directbox.de>
2026-05-01 11:16:47 +0200

    right bar icons

--- polybar/window-icon.sh
+++ polybar/window-icon.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 class=$(xdotool getactivewindow getwindowclassname 2>/dev/null)
 case $class in
-  Brave*|brave*|Chrom*)  echo "%{T3}$(printf '\uf268')%{T-}" ;;
-  kitty*)                echo "%{T3}$(printf '\uf120')%{T-}" ;;
-  Kee*)                  echo "%{T3}$(printf '\uf084')%{T-}" ;;
-  [Nn]vim*|neovim*)      echo "%{T3}$(printf '\ue62b')%{T-}" ;;
-  [Yy]azi*)              echo "%{T3}$(printf '\uf07c')%{T-}" ;;
-  [Ff]irefox*)           echo "%{T3}$(printf '\uf269')%{T-}" ;;
-  *)                     echo "%{T3}$(printf '\uf108')%{T-}" ;;
+  Brave*|brave*|Chrom*)  echo "%{T2}$(printf '\uf268')%{T-}" ;;
+  kitty*)                echo "%{T2}$(printf '\uf120')%{T-}" ;;
+  Kee*)                  echo "%{T2}$(printf '\uf084')%{T-}" ;;
+  [Nn]vim*|neovim*)      echo "%{T2}$(printf '\ue62b')%{T-}" ;;
+  [Yy]azi*)              echo "%{T2}$(printf '\uf07c')%{T-}" ;;
+  [Ff]irefox*)           echo "%{T2}$(printf '\uf269')%{T-}" ;;
+  *)                     echo "%{T2}$(printf '\uf108')%{T-}" ;;
 esac
