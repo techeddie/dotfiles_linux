1cae6aa73975716edaa7c605fd79e02f682d29ca
Eddie <eddie@directbox.de>
2026-05-01 11:31:16 +0200

    fonts and icons

--- polybar/window-icon.sh
+++ polybar/window-icon.sh
@@ -2,9 +2,10 @@
 class=$(xdotool getactivewindow getwindowclassname 2>/dev/null)
 case $class in
   Brave*|brave*|Chrom*)  echo "%{T2}$(printf '\uf268')%{T-}" ;;
-  kitty*)                echo "%{T2}$(printf '\uf120')%{T-}" ;;
+  kitty*|*term*)         echo "%{T2}$(printf '\uf489')%{T-}" ;;
   Kee*)                  echo "%{T2}$(printf '\uf084')%{T-}" ;;
   [Nn]vim*|neovim*)      echo "%{T2}$(printf '\ue62b')%{T-}" ;;
+  obsidian*)             echo "%{T2}$(printf '\uf069')%{T-}" ;;  # or nf-md-asterisk
   [Yy]azi*)              echo "%{T2}$(printf '\uf07c')%{T-}" ;;
   [Ff]irefox*)           echo "%{T2}$(printf '\uf269')%{T-}" ;;
   *)                     echo "%{T2}$(printf '\uf108')%{T-}" ;;
