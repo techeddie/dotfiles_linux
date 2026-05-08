f85f3ab466db1e5e92f1f3ab3f2726cd5d956ae1
Eddie <eddie@directbox.de>
2026-05-08 17:01:29 +0200

    toggle leader e E in snacks explorer

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,8 +1,10 @@
 return {
   "folke/snacks.nvim",
     keys = {
-			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent" },
       -- { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent" },
       { "<leader>ll", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>e", function() Snacks.explorer({ cwd = vim.uv.cwd() }) end, desc = "Explorer (cwd)" },
+      { "<leader>E", function() Snacks.explorer() end, desc = "Explorer (Root Dir)" },
    }
 }
