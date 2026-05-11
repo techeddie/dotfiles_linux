a51465fade6d626c6229b05bd95e748a4e95f229
Eddie <eddie@mbp.local>
2026-05-11 20:33:27 +0200

    rip-substitute - search and replace

---
+++ nvim/lua/plugins/rip-substitute.lua
@@ -0,0 +1,17 @@
+return {
+  "chrisgrieser/nvim-rip-substitute",
+  keys = {
+    {
+      "g/",
+      function() require("rip-substitute").sub() end,
+      mode = { "n", "x" },
+      desc = "Rip Substitute",
+    },
+    {
+      "<leader>sr",
+      function() require("rip-substitute").sub() end,
+      mode = { "n", "x" },
+      desc = "Rip Substitute",
+    },
+  },
+}
