1b6841f12d281a474bbc45839c0538f7c584316e
Eddie <eddie@mbp.local>
2026-05-11 20:33:35 +0200

    hausputz

--- nvim/lua/plugins/.archive/neo-tree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  keys = {
-    -- Swap: e -> cwd, E -> root dir
-    { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)",     remap = true },
-    { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-  },
-}
