0341affbfbaa2b5b48507205ee92f35f13a6b193
Eddie <eddie@directbox.de>
2026-05-16 01:12:26 +0200

    hausputz

--- autokey/data/GENERAL/ALT DOT.py
+++
@@ -1,9 +0,0 @@
-winClass = window.get_active_class()
-print("Window class:", winClass)
-
-if "com.mitchellh.ghostty" in winClass:
-    # Terminal expects Alt+. as ESC + .
-    keyboard.send_keys("<escape>")
-#else:
-    # Regular Alt+. might be sent with real modifier elsewhere
- #   keyboard.send_keys("<alt>.")
--- autokey/data/GENERAL/Alt I.py
+++
@@ -1,5 +0,0 @@
-winClass = window.get_active_class()
-if "Alacritty.Alacritty" in window.get_active_class():
-    keyboard.send_keys("<end>")
-else:
-    keyboard.send_keys("<end>")
