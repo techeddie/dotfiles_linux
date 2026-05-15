92e33885f108fa0d8aa6c1758c10cf004acc51b2
Eddie <eddie@directbox.de>
2026-05-16 00:40:06 +0200

    glance.lua

---
+++ nvim/lua/plugins/glance.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/lua/plugins/glance.lua
+return {
+  "dnlhc/glance.nvim",
+  cmd = "Glance",
+  keys = {
+    { "gD", "<cmd>Glance definitions<cr>",      desc = "Glance definitions" },
+    { "gR", "<cmd>Glance references<cr>",       desc = "Glance references" },
+    { "gY", "<cmd>Glance type_definitions<cr>", desc = "Glance type definitions" },
+    { "gI", "<cmd>Glance implementations<cr>",  desc = "Glance implementations" },
+  },
+}
