cdd0fe2014c97dd8f233452352116b0cfae8cf5a
Eddie <eddie@directbox.de>
2026-05-16 11:54:42 +0200

    ?

--- autokey/data/SHELL/restic/restic snapshots1.py
+++ autokey/data/SHELL/restic/restic snapshots1.py
@@ -2,4 +2,4 @@ import subprocess
 # set clipboard
 subprocess.run(['xclip', '-selection', 'clipboard'], input=b'restic snapshots', check=True)
 # paste via xdotool
-subprocess.run(['xdotool', 'key', '--clearmodifiers', 'ctrl+shift+v'])s
+subprocess.run(['xdotool', 'key', '--clearmodifiers', 'ctrl+shift+v'])
