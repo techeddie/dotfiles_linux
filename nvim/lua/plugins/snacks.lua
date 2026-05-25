22b4e39f1403844ee122ddc19057e246eb6850ca
Eddie <eddie@directbox.de>
2026-05-25 22:57:40 +0200

    recent auto start

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,10 +1,18 @@
 return {
   "folke/snacks.nvim",
-    keys = {
-      -- { "<C-f>", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-			{ "<C-h>", function() Snacks.picker.recent() end, desc = "Recent" },
-      { "<leader>ll", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-      { "<leader>e", function() Snacks.explorer({ cwd = vim.uv.cwd() }) end, desc = "Explorer (cwd)" },
-      { "<leader>E", function() Snacks.explorer() end, desc = "Explorer (Root Dir)" },
-   }
+  keys = {
+    { "<C-h>",        function() Snacks.picker.recent() end,                    desc = "Recent"          },
+    { "<leader>ll",   function() Snacks.picker.lines() end,                     desc = "Buffer Lines"    },
+    { "<leader>e",    function() Snacks.explorer({ cwd = vim.uv.cwd() }) end,   desc = "Explorer (cwd)" },
+    { "<leader>E",    function() Snacks.explorer() end,                         desc = "Explorer (Root)" },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        if vim.fn.argc() == 0 then
+          Snacks.picker.recent()
+        end
+      end,
+    })
+  end,
 }
