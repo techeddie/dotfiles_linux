06864aad92bb00cd975f545283fa1f5c43483d82
Eddie <eddie@directbox.de>
2026-05-25 22:07:31 +0200

    minifiles custom mappings

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -2,8 +2,18 @@ return {
   "nvim-mini/mini.files",
   opts = {
     mappings = {
-      go_in_plus = "<CR>",
-      go_in      = "o",
+      go_in_plus = "o",
+      go_in      = "l",
     },
   },
+  config = function(_, opts)
+    require("mini.files").setup(opts)
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        vim.keymap.set("n", "<CR>", require("mini.files").go_in, { buffer = args.data.buf_id })
+        -- vim.keymap.set("n", "e", require("mini.files").go_in, { buffer = args.data.buf_id })
+      end,
+    })
+  end,
 }
