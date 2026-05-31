6b5dbc053e4f9e945758bd7b4e335ca5b3653e73
Eddie <eddie@directbox.de>
2026-05-31 12:13:26 +0200

    no autostart recent

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,14 +6,15 @@ return {
     { "<leader>ll",   function() Snacks.picker.lines() end,                     desc = "Buffer Lines"    },
     { "<leader>e",    function() Snacks.explorer({ cwd = vim.uv.cwd() }) end,   desc = "Explorer (cwd)" },
     { "<leader>E",    function() Snacks.explorer() end,                         desc = "Explorer (Root)" },
-  },
-  config = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        if vim.fn.argc() == 0 then
-          Snacks.picker.recent()
-        end
-      end,
-    })
-  end,
+  }
+  -- ,
+  -- config = function()
+  --   vim.api.nvim_create_autocmd("VimEnter", {
+  --     callback = function()
+  --       if vim.fn.argc() == 0 then
+  --         Snacks.picker.recent()
+  --       end
+  --     end,
+  --   })
+  -- end,
 }
