c56cca214da55480dc42b3f459e6460f3c532b0c
Eddie <eddie@directbox.de>
2026-06-03 22:41:17 +0200

    markdown headers

--- nvim/lua/config/alias.lua
+++ nvim/lua/config/alias.lua
@@ -16,12 +16,15 @@ cmd [[
     command! KeymapsReload execute "source ~/.config/nvim/lua/config/keymaps.lua" | echo "Keymaps reloaded"
     command! ReloadKeymaps execute "source ~/.config/nvim/lua/config/keymaps.lua" | echo "Keymaps reloaded"
 
+    " OBSIDIAN BASH CODEBLOCKS
     function ObsidianBash()
          :let a = ['', 'bash']
          :%s/```\zs/\=reverse(a)[0]/g
     endfunction
+
     command! BashBlock execute ObsidianBash()
 
+    " OBSIDIAN POWERSHELL CODEBLOCKS
     function! ObsidianPowerShell()
         let a = ['', 'powershell']
         silent! %s/```\zs/\=reverse(a)[0]/g
@@ -29,8 +32,36 @@ cmd [[
         silent! %s/\_s\+\(\n\s*```$\)/\1/g
     endfunction
 
-    command! PowershellBlock call ObsidianPowerShell()
+    command! PowerShellBlock call ObsidianPowerShell()
     command! PSBlock call ObsidianPowerShell()
+
+    function! NormalizeMarkdownHeaders()
+    let l:min_level = 7
+
+    " DECREASE MARKDOWN HEADERS
+    for l:line in getline(1, '$')
+        let l:match = matchstr(l:line, '^#\+\ze\s')
+        if !empty(l:match)
+            let l:level = len(l:match)
+            if l:level < l:min_level
+                let l:min_level = l:level
+            endif
+        endif
+    endfor
+
+    if l:min_level > 1 && l:min_level < 7
+        let l:offset = l:min_level - 1
+        " Ersetze die Rauten durch die (alte Anzahl - Offset)
+        execute 'silent! %s/^#\+\ze\s/\=repeat("#", len(submatch(0)) - ' . l:offset . ')/g'
+        echo "Ueberschriften korrigiert (um " . l:offset . " Ebene(n) angehoben)."
+    else
+        echo "Keine Anpassung noetig."
+    endif
+    endfunction
+
+    command! FixHeaders call NormalizeMarkdownHeaders()
+    command! MarkdownHeadersDecrease call NormalizeMarkdownHeaders()
+    command! DecreaseMarkdownHeaders call NormalizeMarkdownHeaders()
 
 ]]
 
