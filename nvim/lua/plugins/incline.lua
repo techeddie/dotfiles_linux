7da96552bf1e1b05dbcbce055ca856fcd0b001c1
Eddie <eddie@directbox.de>
2026-08-18 20:52:40 +0200

    filename inside buffer

---
+++ nvim/lua/plugins/incline.lua
@@ -0,0 +1,16 @@
+return {
+  "b0o/incline.nvim",
+  event = "VeryLazy",
+  opts = {
+    window = {
+      padding = { left = 1, right = 1 },
+      margin = { horizontal = 0, vertical = 1 },
+      placement = { horizontal = "right", vertical = "top" },
+    },
+    render = function(props)
+      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":.")
+      local modified = vim.bo[props.buf].modified and " ● " or ""
+      return { { modified, guifg = "#f85149" }, { filename, guifg = "#8b949e" } }
+    end,
+  },
+}
